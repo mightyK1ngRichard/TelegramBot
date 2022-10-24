529b210ee8546b9f26f8b398155ebe967c616f8b
Dmitiy <dimapermyakov55@gmail.com>
2022-10-24 13:30:36 +0300

    

--- code.py
+++ code.py
@@ -1,30 +1,35 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
 
 from telebot import TeleBot, types
-
-TOKEN = '5715759447:AAFCFHQ9M1xLp_KRcKKe2Bqk_XORZXXo5vg'
+from main import TOKEN
+from random import randint
 
 bot = TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = [
-        types.KeyboardButton('GitHub'),
-        types.InlineKeyboardButton('VK'),
-        types.InlineKeyboardButton('VK-memes'),
-        types.InlineKeyboardButton('Instagram'),
-        types.InlineKeyboardButton('Gmail')
-    ]
-    markup_reply.add(*buttons)
-
-    bot.send_message(message.chat.id, 'Выберите кнопку:', reply_markup=markup_reply)
+    menu(message, f'Привет, *{message.from_user.first_name} {message.from_user.last_name}*!')
 
 
 @bot.message_handler(content_types=['text'])
 def get_text(message):
-    if message.text == 'GitHub':
+    if message.text == '🎲 Рандомное число':
+        bot.send_message(message.chat.id, 'Выберите диапазон!\n Введите начало(число): ')
+        bot.register_next_step_handler(message, number)
+    elif message.text == '📶 Соц.сети':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = [
+            types.KeyboardButton('GitHub'),
+            types.InlineKeyboardButton('VK'),
+            types.InlineKeyboardButton('VK-memes'),
+            types.InlineKeyboardButton('Instagram'),
+            types.InlineKeyboardButton('Gmail'),
+            types.InlineKeyboardButton('🔙 Назад'),
+        ]
+        markup_reply.add(*buttons)
+        bot.send_message(message.chat.id, 'Выберите соц.сети:', reply_markup=markup_reply)
+    elif message.text == 'GitHub':
         bot.send_message(message.chat.id, 'https://github.com/mightyK1ngRichard')
     elif message.text == 'VK':
         bot.send_message(message.chat.id, 'https://vk.com/mightyk1ngrichard')
@@ -34,8 +39,28 @@ def get_text(message):
         bot.send_message(message.chat.id, 'https://www.instagram.com/permyakoovv/')
     elif message.text == 'Gmail':
         bot.send_message(message.chat.id, 'dimapermyakov55@gmail.com')
+    elif message.text == '🔙 Назад':
+        menu(message, '🔙 Назад')
+
     else:
         bot.send_message(message.chat.id, 'Я тебя не понимаю! Нажми на кнопку!')
+
+
+def menu(message, text):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = [
+        types.KeyboardButton('🎲 Рандомное число'),
+        types.KeyboardButton('📶 Соц.сети'),
+        types.KeyboardButton('📷 Фото'),
+        types.KeyboardButton('🔙 Назад'),
+    ]
+    markup.add(*button)
+    return bot.send_message(message.chat.id, text, reply_markup=markup, parse_mode='markdown')
+
+
+def number(message):
+    number = int(message.text)
+    bot.send_message(message.chat.id, f'Ваша число {randint(0, number)}.')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -2,29 +2,29 @@
 """
 Документация - https://pypi.org/project/pyTelegramBotAPI/
 
-    # message - <class 'telebot.types.Message'>
-    # https://core.telegram.org/bots/api#message
-    # text_for_user = f'Привет, <b> {message.from_user.first_name} {message.from_user.last_name}</b>'
-    # bot.send_message(message.chat.id, text_for_user, parse_mode='html')
+# message - <class 'telebot.types.Message'>
+# https://core.telegram.org/bots/api#message
+# text_for_user = f'Привет, <b> {message.from_user.first_name} {message.from_user.last_name}</b>'
+# bot.send_message(message.chat.id, text_for_user, parse_mode='html')
 """
 
-from telebot import TeleBot, types
+# from telebot import TeleBot, types
+#
+# TOKEN = '5715759447:AAFCFHQ9M1xLp_KRcKKe2Bqk_XORZXXo5vg'
+#
+# bot = TeleBot(TOKEN)
 
-TOKEN = '5758796322:AAGaJvi1_CE7cfoTb2-y-o0MP9d6xYb8Zn8'
 
-bot = TeleBot(TOKEN)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup_inline = types.InlineKeyboardMarkup()
-    buttons = [
-        types.InlineKeyboardButton('Посетить GitHub', url='https://github.com/mightyK1ngRichard'),
-        types.InlineKeyboardButton('Посетить VK', callback_data='VK')
-    ]
-    markup_inline.add(*buttons)
-
-    bot.send_message(message.chat.id, 'GitHub:', reply_markup=markup_inline)
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     markup_inline = types.InlineKeyboardMarkup()
+#     buttons = [
+#         types.InlineKeyboardButton('Посетить GitHub', url='https://github.com/mightyK1ngRichard'),
+#         types.InlineKeyboardButton('Посетить VK', callback_data='VK')
+#     ]
+#     markup_inline.add(*buttons)
+#
+#     bot.send_message(message.chat.id, 'GitHub:', reply_markup=markup_inline)
 
 
 # @bot.message_handler(content_types=['photo'])
@@ -50,14 +50,14 @@ def start(message):
 #     bot.send_message(message.chat.id, 'GitHub:', reply_markup=markup_inline)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def answer(call):
-    if call.data == 'VK':
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = types.KeyboardButton('Мой VK')
-        markup_reply.add(button)
-
-        bot.send_message(call.message.chat.id, 'Нажми кнопку:', reply_markup=markup_reply)
+# @bot.callback_query_handler(func=lambda call: True)
+# def answer(call):
+#     if call.data == 'VK':
+#         markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#         button = types.KeyboardButton('Мой VK')
+#         markup_reply.add(button)
+#
+#         bot.send_message(call.message.chat.id, 'Нажми кнопку:', reply_markup=markup_reply)
 
 
 # markup_reply.
@@ -86,11 +86,11 @@ def answer(call):
 #
 #     elif message.text.split().lower() == 'вэб сайт':
 #         bot.send_message(message.chat.id, 'https://github.com/mightyK1ngRichard')
-@bot.message_handler(content_types=['text'])
-def get_text(message):
-    if message.text == 'Мой VK':
-        bot.send_message(message.chat.id, 'https://github.com/mightyK1ngRichard')
-
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+# @bot.message_handler(content_types=['text'])
+# def get_text(message):
+#     if message.text == 'Мой VK':
+#         bot.send_message(message.chat.id, 'https://github.com/mightyK1ngRichard')
+#
+#
+# if __name__ == '__main__':
+#     bot.polling(none_stop=True)

783de31d1b4ee12178c95f04ea6084dd8188c433
Dmitiy <dimapermyakov55@gmail.com>
2022-10-24 12:08:19 +0300

    

--- code.py
+++ code.py
@@ -9,30 +9,33 @@ bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup_inline = types.InlineKeyboardMarkup()
+    markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = [
-        types.InlineKeyboardButton('Посетить GitHub', url='https://github.com/mightyK1ngRichard'),
-        types.InlineKeyboardButton('Посетить VK', callback_data='VK')
+        types.KeyboardButton('GitHub'),
+        types.InlineKeyboardButton('VK'),
+        types.InlineKeyboardButton('VK-memes'),
+        types.InlineKeyboardButton('Instagram'),
+        types.InlineKeyboardButton('Gmail')
     ]
-    markup_inline.add(*buttons)
+    markup_reply.add(*buttons)
 
-    bot.send_message(message.chat.id, 'GitHub:', reply_markup=markup_inline)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def answer(call):
-    if call.data == 'VK':
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = types.KeyboardButton('Мой VK')
-        markup_reply.add(button)
-
-        bot.send_message(call.message.chat.id, 'Нажми кнопку:', reply_markup=markup_reply)
+    bot.send_message(message.chat.id, 'Выберите кнопку:', reply_markup=markup_reply)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text(message):
-    if message.text == 'Мой VK':
+    if message.text == 'GitHub':
         bot.send_message(message.chat.id, 'https://github.com/mightyK1ngRichard')
+    elif message.text == 'VK':
+        bot.send_message(message.chat.id, 'https://vk.com/mightyk1ngrichard')
+    elif message.text == 'VK-memes':
+        bot.send_message(message.chat.id, 'https://vk.com/iu5memes')
+    elif message.text == 'Instagram':
+        bot.send_message(message.chat.id, 'https://www.instagram.com/permyakoovv/')
+    elif message.text == 'Gmail':
+        bot.send_message(message.chat.id, 'dimapermyakov55@gmail.com')
+    else:
+        bot.send_message(message.chat.id, 'Я тебя не понимаю! Нажми на кнопку!')
 
 
 if __name__ == '__main__':

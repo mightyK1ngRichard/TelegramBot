46dd145e960e6a392f351c9dacca4de1e1c4cf84
Dmitiy <dimapermyakov55@gmail.com>
2022-11-04 00:48:54 +0300

    

--- game.py
+++ game.py
@@ -10,11 +10,12 @@ from telebot import types
 
 
 class Player:
-    def __init__(self, id_user: int, user_name: str, user_role: str | None, choosing_partner: int | None):
-        self.user_id = id_user
+    def __init__(self, user_name: str, user_role: str | None = None, choosing_partner: int | None = None,
+                 counter_for_question: int = 0):
         self.name = user_name
         self.role = user_role
         self.choosing_partner = choosing_partner
+        self.counter_for_question = counter_for_question
 
 
 TOKEN = '1933398269:AAESSDXK_KgOXtqJ0Io_zSfVvNw7BIwKikE'
@@ -22,14 +23,16 @@ TOKEN = '1933398269:AAESSDXK_KgOXtqJ0Io_zSfVvNw7BIwKikE'
 # Список вопросов.
 ALL_QUESTIONS = ['Кого бы Вы взяли с собой в плавание?']
 COUNTER_OF_QUESTIONS = 0  # Счётчик, сколько вопросов уже спросили.
-USERS = []  # Список игроков.
+USERS = {
+    0: Player('')
+}
 
 bot = telebot.TeleBot(token=TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message: types.Message):
-    USERS.append(Player(message.chat.id, user_name=message.from_user.first_name, user_role=None, choosing_partner=None))
+    USERS[message.chat.id] = Player(message.from_user.first_name)
     text = f'Вы добавлены в игру под именем {message.from_user.first_name}'
     bot.send_message(message.chat.id, text)
     print(len(USERS))
@@ -49,11 +52,12 @@ def menu(message: types.Message, text_for_user: str):
 # TODO: решить что-то с юзерами. И вопросами.
 @bot.callback_query_handler(func=lambda call: True)
 def issuing_roles(call: types.CallbackQuery):
+    ID_user = call.message.chat.id
     if call.data == 'главный':
-        global COUNTER_OF_QUESTIONS
         # Создаём кнопки из игроков.
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        buttons = [types.KeyboardButton(el.name) for el in USERS]
+        buttons = [types.KeyboardButton(data.name) for user_id_from_dict, data in USERS.items()
+                   if user_id_from_dict != ID_user]
 
         # Если игрок только один, то ждём.
         if len(buttons) == 1:
@@ -63,50 +67,47 @@ def issuing_roles(call: types.CallbackQuery):
             return
 
         # Проверим, есть ли уже главный игрок.
-        for player in USERS:
-            if player.role == 'Главный игрок':
+        for _, data in USERS.items():
+            if data.role == 'Главный игрок':
                 bot.send_message(call.message.chat.id, 'Главный игрок уже есть!\nВаша роль: второстепенный игрок.')
                 markup.add(*buttons)
-                msg = bot.send_message(call.message.chat.id, text=ALL_QUESTIONS[COUNTER_OF_QUESTIONS],
-                                       reply_markup=markup)
-
-                # TODO: ПОФИКСИТЬ СЧËТЧИК. Чтоб для каждого свой
-                COUNTER_OF_QUESTIONS += 1
-                for user in USERS:
-                    if user.user_id == call.message.chat.id:
-                        user.role = 'Второстепенный игрок'
-                        break
+                msg = bot.send_message(call.message.chat.id,
+                                       text=ALL_QUESTIONS[USERS[ID_user].counter_for_question], reply_markup=markup)
+                USERS[ID_user].counter_for_question += 1
+                USERS[ID_user].role = 'Второстепенный игрок'
                 bot.register_next_step_handler(msg, check_users, 'Второстепенный игрок')
 
         # Если всё хорошо.
         markup.add(*buttons)
-        msg = bot.send_message(call.message.chat.id, text=ALL_QUESTIONS[COUNTER_OF_QUESTIONS], reply_markup=markup)
-        for player in USERS:
-            if player.user_id == call.message.chat.id:
-                player.role = 'Главный игрок'
-                break
-        # TODO: ПОФИКСИТЬ СЧËТЧИК. Чтоб для каждого свой
-        COUNTER_OF_QUESTIONS += 1
+        msg = bot.send_message(call.message.chat.id, text=ALL_QUESTIONS[USERS[ID_user].counter_for_question],
+                               reply_markup=markup)
+        USERS[ID_user].role = 'Главный игрок'
+        USERS[ID_user].counter_for_question += 1
         bot.register_next_step_handler(msg, check_users)
 
     elif call.data == 'второстепенный':
-        pass
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = [types.KeyboardButton(data.name) for user_id_from_dict, data in USERS.items()
+                   if user_id_from_dict != ID_user]
+        markup.add(*buttons)
+        msg = bot.send_message(call.message.chat.id, text=ALL_QUESTIONS[USERS[ID_user].counter_for_question],
+                               reply_markup=markup)
+        USERS[ID_user].counter_for_question += 1
+        USERS[ID_user].role = 'Второстепенный игрок'
+        bot.register_next_step_handler(msg, check_users, 'Второстепенный игрок')
 
 
 def check_users(message: types.Message):
     # Найдём индекс выбранного игрока в списке.
-    index = 0
+    index_user_answer = 0
     answer = message.text
-    for player in USERS:
-        if player.name == answer:
-            index = player.user_id
+    for player_id_from_dict, data in USERS.items():
+        if data.name == answer:
+            index_user_answer = player_id_from_dict
             break
-
-    for player in USERS:
-        if player.user_id == message.chat.id:
-            # Задаём индекс партнёра, которого он выбрал.
-            player.choosing_partner = index
-            break
+    # Задали индекс человека, которого выбрали.
+    USERS[message.chat.id].choosing_partner = index_user_answer
+    print(USERS[index_user_answer].name)
 
 
 if __name__ == '__main__':
--- helpScripts/tests.py
+++ helpScripts/tests.py
@@ -1,31 +1,23 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
-
 import json
-from random import randint
+import re
 
 
-class User:
-    def __init__(self, id_user: int, name: str, age: int, phone_number: str):
-        self.id_user = id_user
-        self.name = name
-        self.age = age
-        self.phone_number = phone_number
-
-
-def add_user() -> dict:
-    data = User(2, input('имя: '), int(input('возраст: ')), input('телефон: '))
-    res = dict()
-    res[data.id_user] = {'name': data.name, 'age': data.age, 'phone_number': data.phone_number}
-    return res
+class Player:
+    def __init__(self, user_name: str, user_role: str | None, choosing_partner: int | None):
+        self.name = user_name
+        self.role = user_role
+        self.choosing_partner = choosing_partner
 
 
 def main():
-    lst = []
-    lst.append(User(0, 'дима', 19, '0930'))
-    lst.append(User(1, 'вова', 19, '33'))
-    for el in lst:
-        if el.name == 'вова':
-            print(el.id_user)
+    lst = {
+        '0': Player('dima', None, None),
+        '1': Player('Vova', None, None),
+    }
+    for el, data in lst.items():
+        if data.name == 'Vova':
+            print(el)
             break
 
 
